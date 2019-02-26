d3e6d6ca765d3cabdca4e81d49ba835c4b4cdf36
Lahiru Rananasinghe <lahiru0561@gmail.com>
2019-02-27 02:10:32 +1100

    Updates

---
+++ precache-manifest.d0fc5b3e18dd2836f520627fe8ce0367.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "82f43a966522558130b4304d23106404",
+    "url": "/videojs-sample/static/media/sample_video.82f43a96.MP4"
+  },
+  {
+    "revision": "786c4bf0c1faaa8456ef20cdad008819",
+    "url": "/videojs-sample/static/media/sample.786c4bf0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/videojs-sample/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4936fc28a16f1bdda51b",
+    "url": "/videojs-sample/static/js/runtime~main.4936fc28.js"
+  },
+  {
+    "revision": "6689e9049e61608f1b03",
+    "url": "/videojs-sample/static/js/main.6689e904.chunk.js"
+  },
+  {
+    "revision": "49c4b2a821fb4c021eef",
+    "url": "/videojs-sample/static/js/2.49c4b2a8.chunk.js"
+  },
+  {
+    "revision": "6689e9049e61608f1b03",
+    "url": "/videojs-sample/static/css/main.a91b2846.chunk.css"
+  },
+  {
+    "revision": "49c4b2a821fb4c021eef",
+    "url": "/videojs-sample/static/css/2.210faa87.chunk.css"
+  },
+  {
+    "revision": "7e5d4596dd70b878c4526ab365c5f72a",
+    "url": "/videojs-sample/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/videojs-sample/precache-manifest.d0fc5b3e18dd2836f520627fe8ce0367.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/videojs-sample/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
