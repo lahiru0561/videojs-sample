6a95c6ef9a2bf7dfe9a55f31dabdc15663224613
Lahiru Rananasinghe <lahiru0561@gmail.com>
2019-02-27 01:28:08 +1100

    sample video and end screen added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,36 +1,36 @@
 import React, { Component } from 'react';
 import Player from './Player';
 
-const recommendations = [
+const sampleData = [
   {
-    name: 'Recom1',
+    name: 'sample1',
     img: '/sample.png',
     url: 'url'
   },
   {
-    name: 'Recom2',
+    name: 'sample2',
     img: '/sample.png',
     url: 'url'
   }
 ];
 export default class Landing extends Component {
   render() {
-    const filmVideoOptions = {
-      autoplay: false,
+    const video_url = require('../files/sample_video.MP4');
+    const videoJsOptions = {
+      autoplay: true,
       controls: true,
       sources: [
         {
-          src: 'URL',
+          src: video_url,
           type: 'video/mp4'
         }
-      ],
-      plugins: {}
+      ]
     };
 
     return (
       <div>
-        Video :
-        <Player {...filmVideoOptions} recomendationData={recommendations} />
+        Sample Video :
+        <Player {...videoJsOptions} endScreenData={sampleData} />
       </div>
     );
   }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
 import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
+import '../../node_modules/video.js/dist/video-js.css';
 
 export default class Player extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isEnd: false
+    };
+  }
+
   componentDidMount() {
     window.videojs = videojs;
-    this.player = videojs(
-      this.videoNode,
-      this.props,
-      function onPlayerReady() {}
-    );
+    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    });
 
     this.player.on('on', () => {
-      // if (this.state.isFilmEnd) {
-      //   this.setState({ isFilmEnd: false });
-      // }
+      if (this.state.isEnd) {
+        this.setState({ isEnd: false });
+      }
     });
 
     this.player.on('ended', () => {
-      // this.setState({ isFilmEnd: true });
-      // this.player.exitFullscreen();
+      this.setState({ isEnd: true });
+      this.player.exitFullscreen();
     });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
   }
 
   render() {
@@ -28,9 +40,31 @@ export default class Player extends Component {
       <div>
         <video
           ref={node => (this.videoNode = node)}
-          className={'video-js-sample'}
+          className="video-js"
           crossOrigin="anonymous"
         />
+        {this.state.isEnd ? (
+          <div className="end-screen-container">
+            <div>
+              <h5 class="end-screen-title">Sample End screen...</h5>
+              <div class="end-screen-items">
+                {this.props.endScreenData.map(item => {
+                  return (
+                    <div className="end-screen-item">
+                      <img
+                        src={require('../files/sample.jpg')}
+                        alt={item.name}
+                      />
+                      <p>{item.name}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
     );
   }
